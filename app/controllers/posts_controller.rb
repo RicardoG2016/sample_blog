eed1c4b0a6bd8b3b58bca183177f533b360c25c7
RicardoG2016 <gomez.r@live.com>
2017-03-10 01:01:14 -0600

    added media query for navbar.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   before_action :authenticate_user!, except: [:index, :show]
 
+def home
+end
+
 def index
   @posts = Post.all.order("created_at DESC")
 end
